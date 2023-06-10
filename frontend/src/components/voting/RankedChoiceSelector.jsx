c7ea09f83d66ef1773ac26b1c7a76acc6443ac32
Rían Errity <rian@paradaux.io>
2023-06-10 16:41:11 +0100

    Add keys to stop warnings

--- frontend/src/components/voting/RankedChoiceSelector.jsx
+++ frontend/src/components/voting/RankedChoiceSelector.jsx
@@ -1,34 +1,34 @@
 import "./RankedChoiceSelector.scss"
 import { getPlacementSuffix } from "../../partials/stringHelper"
+import React from "react"
 
 export default function RankedChoiceSelector({ className, candidates, choices, setChoices }) {
     // TODO, defuckify? Idk it's not that bad and it works...
     return (
         <div className={"ranked-choice-selector " + className}>
             {
-                candidates.map(candidate => (
-                    <>
+                candidates.map((candidate, index) => (
+                    <React.Fragment key={index}>
                         <label htmlFor={candidate.name}>
                             {candidate.name}
-
                         </label>
                         <select 
                             id={candidate.name}
                             onChange={e => {
                                 setChoices(currentChoices => {
                                     let newChoices = {...currentChoices}
-                                    newChoices[candidate.name] = parseInt(e.target.value, 10);
+                                    newChoices[candidate.name] = parseInt(e.target.value, 10) || 1;;
                                     return newChoices;
                                 })
                             }} 
                         >
                             {
-                                candidates.map((x, index) => (
-                                    <option>{`${index + 1}${getPlacementSuffix(index + 1)}`}</option>
+                                candidates.map((x, index2) => (
+                                    <option key={index2}>{`${index2 + 1}${getPlacementSuffix(index2 + 1)}`}</option>
                                 ))
                             }
                         </select>
-                    </>
+                    </React.Fragment>
 
 
                 ))
