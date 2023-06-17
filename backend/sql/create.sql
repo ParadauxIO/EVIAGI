46a598b09fa8655d21e164d3ff34a8bce58de76a
Rían Errity <rian@paradaux.io>
2023-06-17 13:43:33 +0100

    WIP DB Schema

--- backend/sql/create.sql
+++ backend/sql/create.sql
@@ -1,27 +1,70 @@
-CREATE TABLE eviagi_ballots (
-    ballot_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    election_id UUID NOT NULL,
-    choices JSONB,
-    spoiled BOOL
+CREATE TABLE eviagi_organisers (
+    organiser_id UUID PRIMARY KEY REFERENCES auth.users(id),
+    name TEXT NOT NULL,
+    referrer TEXT
 );
 
-CREATE TABLE eviagi_elections (
-    election_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    opened TIMESTAMP,
-    closes TIMESTAMP,
-    has_reopen_nominations BOOL,
-    uses_constituencies BOOL,
-    valid_constituencies UUID[]
-);
-
-CREATE TABLE eviagi_candidates (
-
+CREATE TABLE eviagi_admins (
+    admin_id UUID REFERENCES auth.users(id),
+    name TEXT
 );
 
 CREATE TABLE eviagi_electorate (
-
+    elector_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT,
+    -- If these next two fields are empty the only way they'll be able
+    -- to vote is with a presigned voting link
+    identification_type TEXT, -- Email, discord, mcuuid, etc
+    identification TEXT -- It's left up to the integration to validate this
 );
 
+-- Represents an election.
+CREATE TABLE eviagi_elections (
+    election_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    organiser_id UUID REFERENCES eviagi_organisers(organiser_id),
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    opens TIMESTAMP NOT NULL DEFAULT now(),
+    closes TIMESTAMP,
+    voting_method TEXT NOT NULL,
+    secret_ballot BOOL DEFAULT TRUE,
+    has_reopen_nominations BOOL DEFAULT FALSE,
+    uses_constituencies BOOL DEFAULT TRUE,
+    valid_constituencies UUID[],
+    name TEXT NOT NULL CHECK (length(name) > 0),
+    description TEXT NOT NULL CHECK (length(description) > 0)
+);
+
+-- Represents a given cast ballot.
+-- Optionally stores information about who cast it.
+-- This is for the case of purposefully non-secret ballots
+CREATE TABLE eviagi_ballots (
+    ballot_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    election_id UUID REFERENCES eviagi_elections(election_id),
+    -- Only for non-secret ballots, ordinarily not stored.
+    elector_id UUID REFERENCES eviagi_electorate(elector_id),
+    choices JSONB -- If it's null the vote has been spoiled.
+);
+
+CREATE TABLE eviagi_candidates (
+    elector_id UUID PRIMARY KEY REFERENCES eviagi_electorate(elector_id)
+);
+
+-- The prescence of a record here for a given elector indicates they
+-- have voted in that election.
+CREATE TABLE eviagi_voting_status (
+    elector_id UUID REFERENCES eviagi_electorate (elector_id),
+    election_id UUID REFERENCES eviagi_elections (election_id)
+);
+
+-- The groups of defined groups who can vote in a given election.
 CREATE TABLE eviagi_constituencies (
-
+    constituency_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT
 );
+
+-- Rows indicate an elector is a member of the provided constituency
+CREATE TABLE eviagi_constituency_membership (
+    constituency_id UUID REFERENCES eviagi_constituencies(constituency_id),
+    elector_id UUID REFERENCES eviagi_electorate(elector_id)
+)
+
--- backend/sql/seed.sql
+++ backend/sql/seed.sql
@@ -1,1 +1,7 @@
 -- Seed the database with sample ballots for testing purposes.
+INSERT INTO eviagi_organisers (organiser_id, name) VALUES ('36eb281f-9c92-46f0-8ac4-ca3296df97c2', 'Example Organiser');
+
+INSERT INTO eviagi_elections
+    (organiser_id, opens, closes, voting_method, has_reopen_nominations, valid_constituencies, name, description)
+VALUES ('36eb281f-9c92-46f0-8ac4-ca3296df97c2', now(), now() + INTERVAL '3 days', 'single-transferable-vote', TRUE, '{}', 'Test Election', 'An interesting election');
+
