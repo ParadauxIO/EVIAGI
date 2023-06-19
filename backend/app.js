6697b325b1078ce20c765fc34da9115f4b8ca6af
Rían Errity <rian@paradaux.io>
2023-06-20 00:19:59 +0100

    Fix cors

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const indexRouter = require('./routes/index');
 const electionRouter = require('./routes/elections')
@@ -9,6 +10,7 @@ const electionRouter = require('./routes/elections')
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
