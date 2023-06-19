4b316d3cfeb884268236954d18fb49a24f3c0096
Rían Errity <rian@paradaux.io>
2023-06-19 17:28:48 +0100

    Setup base version of create endpoint

--- backend/routes/elections.js
+++ backend/routes/elections.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { isValidDate } = require('../utils/dateUtils');
+const { authenticateUserMiddleware } = require('../middlewere/supabase-user');
+const { supabase } = require('../utils/supabase')
+const router = express.Router();
 
 // TODO add the middlewere
 
@@ -12,26 +15,90 @@ router.get('/', (req, res, next) => {
   })
 });
 
-router.post('/create', (req, res, next) => {
-    // Needs an auth token
-    
-    // Verify the body contains the necessary fields
+router.post('/create', authenticateUserMiddleware, async (req, res, next) => {
+  let error;
 
-    // Put it to the db
+  if (req.user.aud !== "authenticated") {
+    error = "Invalid user."
+  }
+
+  let form = req.body;
+  form.opens = new Date(form.opens);
+  form.closes = new Date(form.closes);
+
+  // Needs an auth token
+  console.log(req.user)
+
+
+  // Verify the body contains the necessary fields
+  if (form.name.length === 0) {
+    error = "Invalid name.";
+  }
+
+  if (form.description.length === 0) {
+    error = "Invalid description.";
+  }
+
+  if (form.usesConstituencies && form.constituencies.length > 0) {
+    error = "Cannot specify constituencies when useConstituencies is disabled.";
+  }
+
+  if (!isValidDate(form.opens)) {
+    error = "Invalid opening time";
+  }
+
+  if (!isValidDate(form.closes)) {
+    error = "Invalid closing time";
+  }
+
+  if (form.closes < form.opens) {
+    error = "Cannot have a closing time before the opening time.";
+  }
+
+  if (error) {
+    res.status(400);
+    res.json({ error })
+  }
+
+  let { data, error: supabaseError } = await supabase
+    .from("eviagi_elections")
+    .insert({
+      organiser_id: req.user.sub,
+      opens: form.opens,
+      closes: form.closes,
+      voting_method: form.votingMethod,
+      secret_ballot: form.isSecretBallot,
+      has_reopen_nominations: form.hasReopenNominations,
+      uses_constituencies: form.usesConstituencies,
+      valid_constituencies: form.constituencies,
+      name: form.name,
+      description: form.description
+    })
+
+  if (supabaseError) {
+    res.status(400);
+    res.json(supabaseError);
+    return;
+  }
+
+  res.status(201);
+  res.json({ success: "Created election." })
+  // Put it to the db
 })
 
 
 router.get('/:electionId', (req, res, next) => {
-    const {electionId} = req.params;
-    console.log(`getting election: ${electionId}`)
+  const { electionId } = req.params;
+  console.log(`getting election: ${electionId}`)
 
-    // is valid UUID
+  res.send("10");
+  // is valid UUID
 })
 
 router.delete('/:electionId', (req, res, next) => {
-    const {electionId} = req.params;
-    console.log(`deleting election: ${electionId}`)
-    // is valid UUID
+  const { electionId } = req.params;
+  console.log(`deleting election: ${electionId}`)
+  // is valid UUID
 })
 
 
