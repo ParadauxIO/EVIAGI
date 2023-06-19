6fd0c3c33a93ee8cef75dc5a51321c9f86af5df1
Rían Errity <rian@paradaux.io>
2023-06-19 17:28:38 +0100

    Setup supabase auth middleware

--- backend/middlewere/supabase-user.js
+++ backend/middlewere/supabase-user.js
@@ -1,3 +1,6 @@
+const jwt = require("jsonwebtoken")
+
+
 const authenticate = (req, res, next) => {
     // Check if the user is authenticated
     // For example, you can check if a user session exists or if the request contains valid authentication credentials
@@ -9,3 +12,24 @@ const authenticate = (req, res, next) => {
     // User is not authenticated, redirect to the login page or send an error response
     res.redirect('/login');
 };
+
+const authenticateUserMiddleware = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      console.error(err);
+      return res.sendStatus(403);
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = {authenticateUserMiddleware}
